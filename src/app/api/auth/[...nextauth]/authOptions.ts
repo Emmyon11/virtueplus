f6a03fbe416a60be18078616450ff0ab85102dcd
Ogungbola Emmanuel Oluwatosin <emmanuelo1999@gmail.com>
2024-01-18 10:40:41 +0100

    I seperated my authoption

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,52 @@
+import bcrypt from 'bcrypt';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import prisma from '@/lib/primaDB';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        name: { label: 'name', type: 'text' },
+        email: { label: 'email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials || !credentials.email || !credentials.password) {
+          throw new Error('Please enter an email and password');
+        }
+
+        // check to see if user exists
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        // if no user was found
+        if (!user || !user?.hashedPassword) {
+          throw new Error('No user found');
+        }
+
+        // check to see if password matches
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        // if password does not match
+        if (!passwordMatch) {
+          throw new Error('Incorrect password');
+        }
+
+        return user;
+      },
+    }), // ...add more providers here
+  ],
+};
