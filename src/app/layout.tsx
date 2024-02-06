411f18865aa676fa68db62b0a7cc12c4920b355f
Ogungbola Emmanuel Oluwatosin <emmanuelo1999@gmail.com>
2024-02-06 12:02:45 +0100

    added speedisight to my layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter, Tektur, Nunito, Dosis, Mukta } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
